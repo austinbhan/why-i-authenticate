397fbc07c93c14b8f03109b6685ec3b2abc5d486
austinbhan <austinbhan@outlook.com>
2022-05-16 12:42:33 -0700

    Logout button that goes to home page

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,2 +1,9 @@
 // use checkAuth function to redirect is user not authenticated
 // add event listener to the logout button and call logout
+
+const logOut = document.getElementById('logout');
+
+logOut.addEventListener('click', () => {
+    location.replace('/');
+});
+
