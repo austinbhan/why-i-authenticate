442e4ea97baf932259955f85f31b64793fda2e6b
austinbhan <austinbhan@outlook.com>
2022-05-16 12:08:38 -0700

    Functioning Sign Up Form

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { signUpUser } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
 const signInEmail = document.getElementById('sign-in-email');
 const signInPassword = document.getElementById('sign-in-password');
@@ -9,3 +11,13 @@ const signUpPassword = document.getElementById('sign-up-password');
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
+
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    console.log({ email: data.get('email'), password: data.get('password') });
+    const user = await signUpUser(data.get('email'), data.get('password'));
+    if (user) {
+        location.replace('../other-page');
+    }
+});
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://ekvctprnmhlihrtxfomc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrdmN0cHJubWhsaWhydHhmb21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIzMTI1MDcsImV4cCI6MTk2Nzg4ODUwN30.3DwFaO1PRQPJQBj3OEs8ed-pA5qQ_7kbxTiw3YzzplY';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,7 +8,14 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signUpUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    if (response.user) {
+        return response.user;
+    } else {
+        console.error(response.error);
+    }
+}
 
 export async function signInUser(email, password) {}
 
